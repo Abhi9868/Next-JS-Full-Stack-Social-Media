7510c1985ff057025e0cb7356935b6207afeca83
Abhishek Kumar <sahniak9868@gmail.com>
2024-10-26 08:16:57 +0530

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+const Homepage = () => {
+  return (
+    <div className="flex gap-6">
+      <div className="hidden xl:block w-[20%] border border-red-700 min-h-screen"></div>
+      <div className="w-full lg:w-[70%] xl:w-[50%] border border-red-700 min-h-screen"></div>
+      <div className="hidden lg:block w-[30%] border border-red-700 min-h-screen"></div>
+    </div>
+  );
+};
+
+export default Homepage;
